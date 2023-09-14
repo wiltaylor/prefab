f9284d129183fd5c68faf1ce6f72b4171c5a36a6
Wil Taylor <git@wiltaylor.dev>
2023-09-14 16:22:22 +1000

    refactor: remove unused error enum

--- src/errors.rs
+++
@@ -1,8 +0,0 @@
-use thiserror::Error;
-
-#[derive(Error, Debug)]
-pub enum PrefabError {
-    
-    #[error("Can't find template at `{0}`!")]
-    TemplateNotFound(String)
-}
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,2 @@
 pub mod template;
-pub mod errors;
 mod util;
